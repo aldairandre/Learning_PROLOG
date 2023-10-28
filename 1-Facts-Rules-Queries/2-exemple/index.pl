42c67565eee1d16d666a0023139726026c168e8f
aldairandre <aldairandre99@gmail.com>
2023-10-28 06:34:58 +0100

    :heavy_plus_sign: Add exemple 2

---
+++ 1-Facts-Rules-Queries/2-exemple/index.pl
@@ -0,0 +1,8 @@
+pai(pedro,joão).
+pai(pedro,maria).
+idade(pedro,80).
+idade(joão,45).
+idade(maria,35).
+mora(maria,'Frederico Westphalen').
+mora(joão,'Porto Alegre').
+mora(pedro,'Porto Alegre').
